4643e8e90c468a21bbe897825ecdc6840bcf2038
SanchitJain28 <mythichuman28@gmail.com>
2025-04-13 14:21:56 +0530

    feat: update metadata, add sign-in and sign-up pages, and enhance middleware for route protection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import prisma from "@/lib/prisma";
-import { SignIn } from '@clerk/nextjs'
-import { auth } from '@clerk/nextjs/server'
 
 export default async function Home() {
   const users = await prisma.user.findMany();
   
-  const { userId, redirectToSignIn } = await auth()
-  console.log(userId)
-  if (!userId) return redirectToSignIn()
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <SignIn />
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Superblog
       </h1>
