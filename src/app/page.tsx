1f2637ed6984b421c51d1180858ce2f243628b5f
SanchitJain28 <mythichuman28@gmail.com>
2025-04-20 12:52:36 +0530

    refactor: simplify Home component by removing unused user fetching logic and adding loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
-import prisma from "@/lib/prisma";
+"use client";
+import Loading from "@/components/loading";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const users = await prisma.user.findMany();
- 
-  
+export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(true);
+    router.push("/chat");
+    return () => {
+      setLoading(false);
+    };
+  }, []);
+  if(loading){
+    return <Loading/>
+  }
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Superblog
       </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2 text-black">
-            {user.username}
-          </li>
-        ))}
-      </ol>
     </div>
   );
 }
